d4e18b9bc41d49ef7977770f4f1a3adb53a434b7
Sachin <sachin@quintetsolutions.com>
2022-10-29 12:30:46 +0530

    Changes Made for an Interview(ECESIS)

--- js/script.js
+++ js/script.js
@@ -22,25 +22,25 @@ function inVisible(element) {
     //visible in the viewport
     if ((ElementBottom <= WindowBottom) && ElementTop >= WindowTop)
         animate(element);
-    }
+}
 
-    function animate(element) {
-        //Animating the element if not animated before
-        if (!element.hasClass('ms-animated')) {
-            var maxval = element.data('max');
-            var html = element.html();
-            element.addClass("ms-animated");
-            $({
-                countNum: element.html()
-            }).animate({
-                countNum: maxval
-            }, {
-                //duration 5 seconds
-                duration: 5000,
-                easing: 'linear',
-                step: function() {
+function animate(element) {
+    //Animating the element if not animated before
+    if (!element.hasClass('ms-animated')) {
+        var maxval = element.data('max');
+        var html = element.html();
+        element.addClass("ms-animated");
+        $({
+            countNum: element.html()
+        }).animate({
+            countNum: maxval
+        }, {
+            //duration 5 seconds
+            duration: 5000,
+            easing: 'linear',
+            step: function() {
                 element.html(Math.floor(this.countNum) + html);
-                },
+            },
             complete: function() {
             element.html(this.countNum + html);
             }
@@ -59,5 +59,19 @@ $(function() {
             inVisible($(this));
         });
     })
+    
+    var sideMenuOverlay = `<div class="overlay hide"><div/>`
+    $('.global-wrap').append(sideMenuOverlay);
 });
 
+// sideMenu Overlay
+$('.navbar-toggler, .navbar-collapse').mouseover(function (e) {
+    $('.navbar-collapse').addClass('show');
+    $('.overlay').addClass('show');
+    $('.navbar-toggler').removeClass('collapsed');
+});
+$('.navbar-collapse').mouseout(function(e){
+    $('.navbar-collapse').removeClass('show');
+    $('.overlay').removeClass('show');
+    $('.navbar-toggler').addClass('collapsed');
+});
